d83eb8547658423771d8baf229c7b4a1e0463708
Yigit Ak <ak.yigitmesut@gmail.com>
2024-12-07 18:58:35 +0300

    BFS implementation completed

--- src/search_algorithms/BreadthFirstSearch.java
+++ src/search_algorithms/BreadthFirstSearch.java
@@ -2,13 +2,25 @@ package search_algorithms;
 
 import problem_definition.State;
 
+import java.util.ArrayDeque;
+import java.util.List;
+
 public class BreadthFirstSearch extends SearchAlgorithm {
+    private final ArrayDeque<State> queue = new ArrayDeque<>();
+
     public BreadthFirstSearch(State initialState) {
         super(initialState);
     }
 
     @Override
     public void search() {
-        // TODO: implementation
+        State currentState = initialState;
+        do {
+            assert currentState != null;
+            List<State> successors = expand(currentState);
+            applyGoalTest(successors);
+            queue.addAll(successors);
+            currentState = queue.poll();
+        } while(!(isSolutionFound() || queue.isEmpty())); // stop if solution found or queue is empty
     }
 }
