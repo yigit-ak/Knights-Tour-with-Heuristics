64a588c5099bef76643d883df0c2280c706de068
Emir Büçkün <emirbuckun@hotmail.com>
2024-12-11 08:44:12 +0300

    implement dfs

--- src/search_algorithms/DepthFirstSearch.java
+++ src/search_algorithms/DepthFirstSearch.java
@@ -2,13 +2,34 @@ package search_algorithms;
 
 import problem_definition.State;
 
+import java.util.Stack;
+import java.util.List;
+
 public class DepthFirstSearch extends SearchAlgorithm {
+    private final Stack<State> stack = new Stack<>();
+
     public DepthFirstSearch(State initialState) {
         super(initialState);
+        stack.push(initialState); // Initial state is pushed onto the stack
     }
 
     @Override
     public void search() {
-        // TODO: implementation
+        while (!stack.isEmpty() && !isSolutionFound()) {
+            State currentState = stack.pop(); // Pop the most recent state (LIFO)
+            processState(currentState);
+        }
+    }
+
+    private void processState(State currentState) {
+        List<State> successors = expand(currentState);
+        applyGoalTest(successors);
+        pushSuccessorsToStack(successors); // Push successors onto the stack
+    }
+
+    private void pushSuccessorsToStack(List<State> successors) {
+        for (State successor : successors) {
+            stack.push(successor); // Successors are pushed onto the stack
+        }
     }
 }
