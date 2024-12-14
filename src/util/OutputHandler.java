f229d1ca91cb54e39a91dd0e1ef3aee22c91bfcd
Yigit Ak <ak.yigitmesut@gmail.com>
2024-12-14 12:13:36 +0300

    fix on method signature

--- src/util/OutputHandler.java
+++ src/util/OutputHandler.java
@@ -1,6 +1,11 @@
 package util;
 
 import experiment.ExperimentInstance;
+import problem_definition.Location;
+import search_algorithms.SearchAlgorithm;
+
+import java.util.List;
+import java.util.Locale;
 
 public class OutputHandler {
     public static void experimentResultToConsole(ExperimentInstance experiment) {
@@ -13,7 +18,8 @@ public class OutputHandler {
         // TODO: implementation
     }
 
-    public static void solutionPathToFile(String solutionPath) {
+    public static void solutionPathToFile(SearchAlgorithm searchAlgorithm) {
+        List<Location> solutionPath = searchAlgorithm.getSolutionPath();
         // this method prints the solution path in a file
         // the file is to be used for visualization of the solution
         // the format should be like: 0, 0 \n 2, 1 \n 4, 2 \n ...
