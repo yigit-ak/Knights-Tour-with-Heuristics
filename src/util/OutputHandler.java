ca73ff35affd75acc6a4ef331764260277490f2f
Yigit Ak <ak.yigitmesut@gmail.com>
2024-12-14 12:11:12 +0300

    output handler class's template added

---
+++ src/util/OutputHandler.java
@@ -0,0 +1,22 @@
+package util;
+
+import experiment.ExperimentInstance;
+
+public class OutputHandler {
+    public static void experimentResultToConsole(ExperimentInstance experiment) {
+        // this method prints the experiment result on the console
+        // TODO: implementation
+    }
+
+    public static void experimentResultToFile(ExperimentInstance experiment) {
+        // this method prints the experiment result in a file
+        // TODO: implementation
+    }
+
+    public static void solutionPathToFile(String solutionPath) {
+        // this method prints the solution path in a file
+        // the file is to be used for visualization of the solution
+        // the format should be like: 0, 0 \n 2, 1 \n 4, 2 \n ...
+        // TODO: implementation
+    }
+}
