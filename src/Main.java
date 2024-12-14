dcd045f381760061b5a4662fcd36ed5658dda11d
Yigit Ak <ak.yigitmesut@gmail.com>
2024-12-14 11:45:32 +0300

    experiment setup is done

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,37 @@
+import problem_definition.Location;
+import problem_definition.State;
 import search_algorithms.*;
-import problem_definition.*;
 
-import java.util.Scanner;
+import java.lang.reflect.Constructor;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
 
-        System.out.print("Enter board size (n): ");
-        int n = scanner.nextInt();
-
-        System.out.print("Enter search method (a-d): ");
-        char method = scanner.next().charAt(0);
-
-        System.out.print("Enter time limit (t) in seconds: ");
-        int timeLimit = scanner.nextInt();
-
-        scanner.close();
-
-        SearchAlgorithm searchAlgorithm = createSearchAlgorithm(n, method);
-
-        long startTime = System.currentTimeMillis();
-        searchAlgorithm.search();
-        long endTime = System.currentTimeMillis();
-
-        printResults(searchAlgorithm, method, timeLimit, endTime - startTime);
-    }
-
-    private static SearchAlgorithm createSearchAlgorithm(int n, char method) {
-        State initialState = new State(new boolean[n][n], new Location(0, 0));
-        return switch (method) {
-            case 'a' -> new BreadthFirstSearch(initialState);
-            case 'b' -> new DepthFirstSearch(initialState);
-            case 'c' -> new DfsWithWarnsdorffRule(initialState);
-            case 'd' -> new DfsWithEnhancedWarnsdorffRule(initialState);
-            default -> throw new IllegalArgumentException("Invalid search method");
+        final Location START_LOCATION = new Location(0, 0);
+        int[] boardSizes = {8, 16, 32, 41, 52};
+        Class<?>[] searchAlgorithmClasses = {
+                BreadthFirstSearch.class,
+                DepthFirstSearch.class,
+                DfsWithWarnsdorffRule.class,
+                DfsWithEnhancedWarnsdorffRule.class
         };
+
+        for (Class<?> searchAlgorithmClass : searchAlgorithmClasses) {
+            for (int boardSize : boardSizes) {
+                boolean[][] board = new boolean[boardSize][boardSize];
+                State initialState = new State(board, START_LOCATION);
+
+                Constructor<?> constructor = searchAlgorithmClass.getDeclaredConstructor(State.class);
+                SearchAlgorithm searchAlgorithm = (SearchAlgorithm) constructor.newInstance(initialState);
+
+                ExperimentInstance experimentInstance = new ExperimentInstance(searchAlgorithm);
+                experimentInstance.runSearch();
+            }
+        }
+
     }
 
-    private static void printResults(SearchAlgorithm searchAlgorithm, char method, int timeLimit, long timeSpent) {
+    private static void printResults(ExperimentInstance experimentInstance) {
         System.out.println("Search method: " + method);
         System.out.println("Time limit: " + timeLimit + " seconds");
 
@@ -47,11 +40,7 @@ public class Main {
             System.out.println("Solution path: " + searchAlgorithm.getSolutionPath());
             System.out.println("Time spent: " + timeSpent + " milliseconds");
         } else {
-            System.out.println("No solution exists.");
+            System.out.println("No solution found within time limit.");
         }
-
-        // TODO: Implement this method in SearchAlgorithm.java
-        // System.out.println("Number of nodes expanded: " +
-        // searchAlgorithm.getNumberOfNodesExpanded());
     }
 }
