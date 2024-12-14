eb09a042d3871cde055eef92a7ca7f5e11c3fcca
Yigit Ak <ak.yigitmesut@gmail.com>
2024-12-14 12:05:00 +0300

    experiment instance class

---
+++ src/experiment/ExperimentInstance.java
@@ -0,0 +1,16 @@
+package experiment;
+
+import search_algorithms.SearchAlgorithm;
+
+public class ExperimentInstance {
+    final SearchAlgorithm searchAlgorithm;
+
+    public ExperimentInstance(SearchAlgorithm searchAlgorithm) {
+        this.searchAlgorithm = searchAlgorithm;
+    }
+
+    public void runSearch() {
+        long startTime = System.currentTimeMillis();
+        searchAlgorithm.search();
+    }
+}
