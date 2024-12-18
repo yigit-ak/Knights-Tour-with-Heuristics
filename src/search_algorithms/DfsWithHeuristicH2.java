c49233a25691a06155c9b95cf192964a4fde437d
Emir Büçkün <emirbuckun@hotmail.com>
2024-12-18 17:53:30 +0300

    edit calculation functions

--- src/search_algorithms/DfsWithHeuristicH2.java
+++ src/search_algorithms/DfsWithHeuristicH2.java
@@ -27,12 +27,12 @@ public class DfsWithHeuristicH2 extends SearchAlgorithm {
 
             // Apply heuristic h2: sort successors by Warnsdorff's rule,
             // breaking ties by distance to corners
-            successors = new ArrayList<>(successors);                             // Ensure the list is mutable
+            successors = new ArrayList<>(successors); // Ensure the list is mutable
             successors.sort(
-                    Comparator                                                    // Comparator sorts in ascending order
-                            .comparingInt(this::retrieveNumberOfAvailableMoves)   // Apply warnsdorff's rule (as in h1b)
-                            .thenComparingInt(this::retrieveDistanceToCorners)    // Break ties if equal (h2)
-                            .reversed()     // Reverse the order to pop smaller values from stack first (LIFO)
+                    Comparator // Comparator sorts in ascending order
+                            .comparingInt(this::calculateNumberOfAvailableMoves) // Apply warnsdorff's rule (as in h1b)
+                            .thenComparingInt(this::calculateDistanceToCorners) // Break ties if equal (h2)
+                            .reversed() // Reverse the order to pop smaller values from stack first (LIFO)
             );
 
             // Push sorted successors onto the stack
@@ -41,27 +41,15 @@ public class DfsWithHeuristicH2 extends SearchAlgorithm {
         }
     }
 
-    private int retrieveNumberOfAvailableMoves(State state) {
+    private int calculateNumberOfAvailableMoves(State state) {
         Location lastPlacedKnight = state.locationOfLastPlacedKnight();
         List<Location> availableMoves = lastPlacedKnight.getLocationsForNextMove(state.board());
         return availableMoves.size(); // Number of onward moves (lower is better)
     }
 
-    // to apply h2
-    private int retrieveDistanceToCorners(State state) {
-        // Distance to corners of the board (lower is better)
-        return calculateDistanceToCorners(state.locationOfLastPlacedKnight(), state.board().length);
-    }
-
-    /**
-     * Calculates the minimum Manhattan distance from a location to any corner of
-     * the board.
-     *
-     * @param location  The current knight's location.
-     * @param boardSize The size of the board (n x n).
-     * @return The minimum distance to any corner.
-     */
-    private int calculateDistanceToCorners(Location location, int boardSize) {
+    private int calculateDistanceToCorners(State state) {
+        Location location = state.locationOfLastPlacedKnight();
+        int boardSize = state.board().length;
         int row = location.row();
         int col = location.column();
 
